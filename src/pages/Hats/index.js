eb2098ce75b0eac7c84c432ba6cd145e89c19be4
Bianca Cunha <biancunha@gmail.com>
2021-05-02 17:46:54 -0300

    updating app with a shop page, collection preview and collection item components

--- src/pages/Hats/index.js
+++
@@ -1,7 +0,0 @@
-export default function Hats() {
-    return (
-        <div>
-            <h1>HATS PAGE</h1>
-        </div>
-    );
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import Hats from '../pages/Hats';
+import Shop from '../pages/Shop';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
-                <Route path="/hats" exact component={Hats} />
+                <Route path="/shop" exact component={Shop} />
             </Switch>
         </BrowserRouter>
     );
